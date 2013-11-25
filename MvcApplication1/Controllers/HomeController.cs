50ee18583093a6eacea490993d214a29ff049885
dawu17 <bluedaniel86@hotmail.com>
2013-11-25 15:43:58 -0800

    firstuploadtest

---
+++ MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace IS445.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult WebForm()
+        {
+            return View();
+        }
+
+        public ActionResult Aboutme()
+        {
+            return View();
+        }
+
+
+    }
+}
